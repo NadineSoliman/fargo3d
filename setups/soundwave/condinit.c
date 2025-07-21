368f84ec1952e7b882cbb1462e36f85e785f2bf2
Nadine Soliman <23432485+NadineSoliman@users.noreply.github.com>
2025-07-21 15:57:20 -0400

    fixing sign problem in Tdust

--- setups/soundwave/condinit.c
+++ setups/soundwave/condinit.c
@@ -17,10 +17,10 @@ void _CondInit(int index) {
 
 
   //Input parameters
-  real Ts[NFLUIDS-1] = { 0.1 , 0.21544346900318834 , 0.46415888336127786 , 1.0 };
-  real Th[NFLUIDS-1] = { 0.1 , 0.21544346900318834 , 0.46415888336127786 , 1.0 };
-  real eps[NFLUIDS-1] = { 0.1 , 0.23333333333333334 , 0.3666666666666667 , 0.5 };
-  double complex lambda =  -0.548547 + -7.34385 *I; 
+  real Ts[NFLUIDS-1] = { 0.1 };//, 0.21544346900318834 , 0.46415888336127786 , 1.0 };
+  real Th[NFLUIDS-1] = { 0.1};// , 0.21544346900318834 , 0.46415888336127786 , 1.0 };
+  real eps[NFLUIDS-1] = { 0.1}; //, 0.23333333333333334 , 0.3666666666666667 , 0.5 };
+  double complex lambda =  -0.18186145573318502 - 7.16601928447143877*I; 
   
  #ifdef CONSTANTTHERMALCOEFF
   if(index > 0 ) Coeffval[1] = 1.0/Th[index-1];
@@ -31,22 +31,22 @@ void _CondInit(int index) {
   double complex rhodust[NFLUIDS-1];
   double complex Tdust[NFLUIDS-1];
   double complex edust[NFLUIDS-1];
-  double complex delta_rhog = DENSGR;
-  double complex vgas       = -I*lambda/kmode*delta_rhog/RHOG;
+  double complex delta_rhog = DENSGR * RHOG;
+  double complex vgas       = -I*lambda/(CS*kmode)*delta_rhog/RHOG * CS;
 
   double complex sum=0.0;
     for(m=0;m<NFLUIDS-1;m++){
       sum += eps[m] / (1.0 + lambda*Ts[m]);
   }
-  double complex delta_Tg = - (1 - (((lambda * lambda) / (CS * kmode * CS * kmode)) *(1 + sum))) * delta_rhog / RHOG;
-  double complex delta_e = (delta_rhog / RHOG) + (delta_Tg/TGAS);
+  double complex delta_Tg = - (1 - (((lambda * lambda) / (CS * kmode * CS * kmode)) *(1 + sum))) * delta_rhog / RHOG * TGAS;
+  double complex delta_e = CP_GAS * (delta_rhog  * TGAS) + (delta_Tg * RHOG);
 
   for(m=0;m<NFLUIDS-1;m++){
 
     Tdust[m]   =  - 1/ (1 + lambda*Th[m]) * delta_Tg;
-    vdust[m]   = -I*lambda/(CS * kmode) / (1 + lambda *Ts[m])*  delta_rhog / RHOG;
-    rhodust[m] =  eps[m]/(1.0+lambda*Ts[m])*delta_rhog / RHOG;
-    edust[m]   = CP_DUST/CP_GAS * (rhodust[m]  + Tdust[m]);
+    vdust[m]   = vgas/ (1 + lambda *Ts[m]);
+    rhodust[m] =  eps[m]/(1.0+lambda*Ts[m])*delta_rhog;
+    edust[m]   = CP_DUST * (rhodust[m] * TGAS  + Tdust[m] * RHOG * eps[m]);
 
   }
   
@@ -56,17 +56,23 @@ void _CondInit(int index) {
     if (index == 0) {
       rho[k] = RHOG + AMPLITUDE*mode( DENSGR, DENSGI, kmode, zmed(k) );
       v[k]   = AMPLITUDE*mode( creal(vgas), cimag(vgas), kmode, zmin(k) );
+#ifdef ADIABATIC
       e[k]  = (RHOG * TGAS * CP_GAS);
-      e[k] += AMPLITUDE*mode( creal(delta_e), cimag(delta_e), kmode, zmin(k) );
+      e[k] +=  AMPLITUDE*mode( creal(delta_e), cimag(delta_e), kmode, zmin(k) );
+#else
+      e[k]  = CS;
+#endif
     }
     else {
       rho[k]  = eps[index-1]*RHOG;
       rho[k] += AMPLITUDE*mode( creal(rhodust[index-1]), cimag(rhodust[index-1]), kmode, zmed(k) );
       v[k]    = AMPLITUDE*mode( creal(vdust[index-1])  , cimag(vdust[index-1])  , kmode, zmin(k) );
-      printf("v = %f \n", v[k]);
-
+#ifdef ADIABATIC
       e[k]   = TDUST * CP_DUST * eps[index-1] * RHOG;
       e[k]  += AMPLITUDE*mode( creal(edust[index-1]), cimag(edust[index-1]), kmode, zmed(k) );
+#else
+      e[k]   =0;
+#endif
     }
   }
 }
